7274cdb47c53457fa079445b5a278b8770215778
Julien TANG <122900891+julientang@users.noreply.github.com>
2025-02-28 17:31:00 -0800

    Update pip-pkgs.sh

--- conf/pip-pkgs.sh
+++ conf/pip-pkgs.sh
@@ -5,6 +5,16 @@ echo Installing pip packages at $(date)
 pip install --force --no-cache-dir --no-binary=mpi4py mpi4py
 pip install cython
 
+git clone git@github.com:carronj/plancklens.git
+git checkout plancklensdev
+cd plancklens
+pip install .
+
+git clone git@github.com:NextGenCMB/delensalot.git
+cd delensalot
+python3 -m pip install -r requirements.txt .
+python3 setup.py develop
+
 if [ $? != 0 ]; then
     echo "ERROR installing pip packages; exiting"
     exit 1
