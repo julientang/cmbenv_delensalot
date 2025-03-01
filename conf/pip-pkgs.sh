9cd74626e74cbe728e783abf32037ab7418f4d9c
Julien TANG <122900891+julientang@users.noreply.github.com>
2025-02-28 16:58:28 -0800

    Update pip-pkgs.sh

--- conf/pip-pkgs.sh
+++ conf/pip-pkgs.sh
@@ -1,16 +1,9 @@
 # Install pip packages.
 echo Installing pip packages at $(date)
 
-# JAX install follows: https://docs.nersc.gov/development/languages/python/using-python-perlmutter/#jax
-pip install --no-cache-dir "jax[cuda12]" "jaxlib" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
-
 # see https://docs.nersc.gov/development/languages/python/parallel-python/
 pip install --force --no-cache-dir --no-binary=mpi4py mpi4py
 pip install cython
-
-# git clone git@github.com:1cosmologist/skytools.git skytools
-# cd skytools
-# pip install --no-dependencies .
 
 if [ $? != 0 ]; then
     echo "ERROR installing pip packages; exiting"
